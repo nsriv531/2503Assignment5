4f0f2389c32d96b997ebb88e3c5d9ac327b44d88
nsriv531 <77025122+nsriv531@users.noreply.github.com>
2022-12-07 17:58:48 -0700

    Add files via upload

---
+++ src/model/HashElement.java
@@ -0,0 +1,10 @@
+package model;
+
+public class HashElement {
+
+private String word; //key for the hash table
+private int count;
+
+
+
+}
---
+++ src/model/WordCounter.java
@@ -0,0 +1,35 @@
+package model;
+
+public class WordCounter implements HashInterface<HashElement>{
+
+	@Override
+	public int gethashCode(HashElement key) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void put(HashElement key) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public HashElement remove(HashElement key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printTable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
