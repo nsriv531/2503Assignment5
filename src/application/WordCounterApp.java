c54172a1192b2e695d0dadd87e70e000f5223ba2
nashdashin <77025122+nashdashin@users.noreply.github.com>
2022-12-07 20:58:41 -0700

    changes have been made to the assignment

--- src/application/WordCounterApp.java
+++ src/application/WordCounterApp.java
@@ -1,15 +1,44 @@
 package application;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import model.WordCounter;
+
 public class WordCounterApp {
-
-	Scanner input = new Scanner(System.in);
 	
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner input = new Scanner(System.in);
+		String fileName = input.nextLine();
+		File fileToRead = new File(fileName);
+		WordCounter instanceCounter;
 		
 		System.out.printf("Enter the name of the text file: ");
-		input.nextLine();
+		System.out.printf("Enter the size of the hashtable: ");
+		int sizeOfTable = input.nextInt();
+		instanceCounter = new WordCounter(sizeOfTable);
+		
+		
+		try(Scanner fileReader = new Scanner(fileToRead)){
+			
+			String currentLine;
+			String [] splittedLine;
+			char asciiValue;
+			
+			while(fileReader.hasNextLine()) {
+				
+				currentLine = fileReader.nextLine().toLowerCase();
+				instanceCounter.put(null);
+				
+				splittedLine = currentLine.split(" ");
+				
+			}
+			
+			
+			fileReader.close();
+		}
+		
+		
 		
 		
 		
